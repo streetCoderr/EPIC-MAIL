e7af4b88068df76ce08e534e116ad37da2e06e88
streetCoder <streetcoder99@gmail.com>
2022-07-16 08:03:35 +0100

    feat: set up database connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 const express = require("express")
 
+const connectDB = require("./db/connect")
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
@@ -8,6 +10,7 @@ const PORT = process.env.PORT || 3000
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(PORT, () => {
       console.log(`server is listening at http://localhost:${PORT}`)
     })
---
+++ db/connect.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const connectDB = (URI) => mongoose.connect(URI);
+
+module.exports = connectDB
