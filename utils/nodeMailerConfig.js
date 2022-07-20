3377ccded6c8a1b86ebe6cd79646b962bf3574c5
streetCoder <streetcoder99@gmail.com>
2022-07-20 11:19:38 +0100

    feat: replace fake email tester with gmail API

--- utils/nodeMailerConfig.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-module.exports = {
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: process.env.MAILTRAP_USER,
-    pass: process.env.MAILTRAP_PASS
-  }
-}
