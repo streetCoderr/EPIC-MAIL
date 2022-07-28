7e32afbcb23daf631d0283e705c87dd000b312bc
streetCoderr <streetcoder99@gmail.com>
2022-07-28 11:37:52 +0100

    chore: set up schema for Messages

---
+++ model/message.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const MessageSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    default: "no-title",
+    trim: true,
+    maxlength: 50
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ['draft', 'sent', 'retracted'],
+      message: "{VALUE} is not a valid status"
+    },
+    required: true,
+    default: 'draft',
+  },
+  sentDate: Date,
+}, {timestamps: true})
+
+module.exports = mongoose.model('Message', MessageSchema)
